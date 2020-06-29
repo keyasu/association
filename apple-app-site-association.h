20935ce5ce3971ec3d24800dbefde6080bd1e31f
wangergang <249804175@qq.com>
2020-06-29 10:40:31 +0800

    first commit

---
+++ apple-app-site-association.h
@@ -0,0 +1,11 @@
+{
+    "applinks": {
+        "apps": [],
+        "details": [
+            {
+                "appID": "L2Z94BFC7Q.doulanlive.doulan",
+                "paths": [ "*" ]
+            }
+        ]
+    }
+}
